cea09fe1335ab069d45514d7c2876c655bd7f0ea
Zeynep Sıla Kaymak <72812386+zeynepsila@users.noreply.github.com>
2025-03-18 10:29:16 +0300

    Add files via upload

---
+++ slim_paranomi/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'paranomi_db',
+        'user' => 'root',
+        'password' => ''
+    ],
+    'jwt' => [
+        'secret' => 'SECRET_KEY'
+    ]
+];
